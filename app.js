43b939601137ff67ad9f42d8bddb189ea5c819ff
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-10-08 21:58:16 +0700

    Refactor dependencies in package.json and update CORS handling in app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express, { json } from "express";
 import { reqLogger } from "./helper/mdw.js";
+import dotenv from "dotenv";
 const app = express();
 const port = 3000;
 
+dotenv.config();
+
 const allowedOrigins = ["http://localhost:2000"]; // Allowed origins
+const api = process.env.API || "http://localhost:3000";
 
 // Enable CORS for all routes (standard settings)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "origin");
+  res.header("Access-Control-Allow-Origin", "http://localhost:2000");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
@@ -28,7 +32,67 @@ app.get("/ping", (req, res) => {
   res.json({ message: "Hello from API!" });
 });
 
-app.all("/api/data", (req, res) => {
+app.get("/req-get-ping", async (req, res) => {
+  try {
+    const resp = await fetch(`${api}/ping`, {
+      method: "GET",
+    });
+    const data = await resp.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get("/req-post-ping", async (req, res) => {
+  try {
+    const resp = await fetch(`${api}/post-ping`, {
+      method: "POST",
+    });
+    const data = await resp.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get("/req-put-ping", async (req, res) => {
+  try {
+    const resp = await fetch(`${api}/put-ping`, {
+      method: "PUT",
+    });
+    const data = await resp.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get("/req-delete-ping", async (req, res) => {
+  try {
+    const resp = await fetch(`${api}/delete-ping`, {
+      method: "DELETE",
+    });
+    const data = await resp.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get("/req-patch-ping", async (req, res) => {
+  try {
+    const resp = await fetch(`${api}/patch-ping`, {
+      method: "PATCH",
+    });
+    const data = await resp.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+app.post("/api/data", (req, res) => {
   res.json({ received: req.body });
 });
 
