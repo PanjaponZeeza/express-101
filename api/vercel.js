aed146cee2c4bee761213b3c9379960ad6a60570
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-10-08 21:29:28 +0700

    Refactor file paths and update CORS handling in app.js

--- api/vercel.js
+++
@@ -1,8 +0,0 @@
-import { createServer } from "http";
-import { parse } from "url";
-import app from "./app";
-
-export default (req, res) => {
-  const parsedUrl = parse(req.url, true);
-  app.handle(req, res, parsedUrl);
-};
--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 import express, { json } from "express";
-import { PrismaClient } from "@prisma/client";
-import { reqLogger } from "./../helper/mdw.js";
+import { reqLogger } from "./helper/mdw.js";
 const app = express();
-
-const prisma = new PrismaClient();
+const port = 3000;
 
 const allowedOrigins = ["http://localhost:2000"]; // Allowed origins
 
 // Enable CORS for all routes (standard settings)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:2000");
+  res.header("Access-Control-Allow-Origin", "origin");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
@@ -34,20 +32,8 @@ app.all("/api/data", (req, res) => {
   res.json({ received: req.body });
 });
 
-app.post("/api/user/create", async (req, res) => {
-  const { name, email } = req.body;
-
-  // Equivalent to: INSERT INTO User (name, email) VALUES ('name', 'email');
-  const newUser = await prisma.user.create({
-    data: { name, email },
-  });
-
-  if (!newUser) {
-    return res.status(400).json({ error: "User not created." });
-  }
-  return res
-    .status(200)
-    .json({ result: `User name ${name}, email ${email} had created.` });
+app.listen(port, () => {
+  console.log(`API running on http://localhost:${port}`);
 });
 
 export default app;
